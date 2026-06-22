2c617b82cfd5e32b2b12d50f8fa6505be3de6fd8
Kanero Juniar <129026887+Kanero095@users.noreply.github.com>
2026-06-22 08:49:10 +0700

    Update Schedule

--- routes/console.php
+++ routes/console.php
@@ -200,5 +200,5 @@ Artisan::command('mail:diagnose {--email= : Email tujuan untuk tes pengiriman}',
 // --- Scheduling Definitions ---
 
 Schedule::command('missions:generate')->dailyAt('00:00');
-Schedule::command('notifications:morning')->dailyAt('08:00');
+Schedule::command('notifications:morning')->dailyAt('08:55');
 Schedule::command('notifications:evening')->dailyAt('20:00');
